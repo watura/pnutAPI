afbf10680135b86506ef56fd7bfb46ed3d6d1cc9
Wataru NISHIMOTO <watura@g91.org>
2018-06-02 07:18:45 +0900

    Project Initial commit

---
+++ pnutAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pnutAPI
+//
+//  Created by Wataru Nishimoto on 2018/6/2.
+//  Copyright © 2018 Wataru Nishimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
