77c20fd31eced331450e543f60963d8af835ae51
Wataru NISHIMOTO <watura@g91.org>
2018-06-02 09:56:26 +0900

    Able to get oauth token

--- pnutAPI/AppDelegate.swift
+++ pnutAPI/AppDelegate.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import OAuthSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
+        if url.host == "pnut-callback" {
+            OAuthSwift.handle(url: url)
+        }
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -43,4 +50,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- pnutAPI/ViewController.swift
+++ pnutAPI/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class ViewController: UIViewController {
+    var oauthswift: OAuth2Swift?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchUpInside(_ sender: Any) {
+        guard let filePath = Bundle.main.path(forResource: "Info", ofType: "plist" ),
+            let plist = NSDictionary(contentsOfFile: filePath) else {
+                fatalError("Could not load plist")
+        }
+        guard let clientId = plist["PnutClientID"] as? String,
+            let clientSecret = plist["PnutClientSecret"] as? String else {
+                fatalError("Could not read secrets")
+        }
 
+        oauthswift = OAuth2Swift(
+            consumerKey: clientId,
+            consumerSecret: clientSecret,
+            authorizeUrl: "https://pnut.io/oauth/authenticate",
+            responseType: "token"
+        )
+        guard let oauthswift = oauthswift else {
+            fatalError()
+        }
+
+        oauthswift.authorizeURLHandler = SafariURLHandler(viewController: self, oauthSwift: oauthswift)
+        oauthswift.authorize(
+            withCallbackURL: URL(string: "pnut-callback://pnut-callback/pnut")!,
+            scope: "basic+stream+write_post+follow+update_profile+presence+messages+files+polls", state: "",
+            success: { credential, response, parameters in
+                let ud = UserDefaults.standard
+                ud.set(credential.oauthToken, forKey: "OAuthToken")
+                ud.synchronize()
+                print(credential.oauthToken)
+        },
+            failure: { error in
+                print(error.localizedDescription)
+        }
+        )
+    }
 }
-
