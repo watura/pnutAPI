a06045202ee1b7df0ba07d2d7888c28a68ba2c43
Wataru NISHIMOTO <watura@g91.org>
2018-07-08 20:46:47 +0900

    [WIP]

--- Sources/Components/ProfileViewController/ProfileViewController.swift
+++ Sources/Components/ProfileViewController/ProfileViewController.swift
@@ -10,3 +10,21 @@ extension ProfileViewController: StoryboardedType {
     public static var storyboardName: String { return "ProfileView" }
     public static var identifier: String { return "ProfileView" }
 }
+
+
+public class IconViewController: UIViewController {
+    var iconUrl: URL!
+    @IBOutlet weak var imageView: UIImageView!
+}
+
+public class HeaderImageViewController: UIViewController {
+    var imageUrl: URL!
+    @IBOutlet weak var imageView: UIImageView!
+}
+
+public class CircleBorderedButtonViewController: UIViewController {
+    var text: String!
+    var onPress: (() -> Void)!
+}
+
+public class BioViewController: UIViewController {}
